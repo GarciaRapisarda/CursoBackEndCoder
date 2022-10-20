ea95fdb489751a2ad969bdc93821b842fa83e3a8
Gonzalo Garcia Rapisarda <99925581+GarciaRapisarda@users.noreply.github.com>
2022-10-20 10:57:14 -0300

    Creando Estructura

---
+++ Segunda-Entrega-ProyectoFinal/src/controllers/productManager.js
@@ -0,0 +1,63 @@
+const { timeStamp } = require('console');
+const fs = require('fs');
+
+const productsFilePath = './src/data/products.json';
+
+class ProductManager {
+    findAll = async () => {
+        if (!fs.existsSync(productsFilePath)) 
+            return { error: 'No hay productos cargados' }
+            let data = await fs.promises.readFile(productsFilePath, 'utf-8');
+            return JSON.parse(data);
+        }
+    findById = async (id) => {
+        if (!id) return { status: "error", message: "Id no encontrado" };
+        if (fs.existsSync(productsFilePath)) {
+            let data = await fs.promises.readFile(productsFilePath, 'utf-8');
+            let products = JSON.parse(data);
+            let product = products.find(product => product.id == id);
+            if (product) return { status: "success", data: product };
+            else return { status: "error", message: "Producto no encontrado" };
+        } else return { status: "error", message: "No hay productos cargados" };
+    }
+    saveProduct =  async (product) => {
+        if (!product) return { status: "error", message: "Producto no encontrado" };
+        if (fs.existsSync(productsFilePath)) {
+            let data = await fs.promises.readFile(productsFilePath, 'utf-8');
+            let products = JSON.parse(data);
+            let id = products.length + 1;
+            product.id = id;
+            products.push(product);
+            await fs.promises.writeFile(productsFilePath, JSON.stringify(products, null, '\t'));
+            return { status: "success", data: product };
+        } else return { status: "error", message: "No hay productos cargados" };
+    }
+    updateProduct = async (id, product) => {
+        if (!id) return { status: "error", message: "Id no encontrado"};
+        if (fs.existsSync(productsFilePath)) {
+            let data = await fs.promises.readFile(productsFilePath, 'utf-8');
+            let products = JSON.parse(data);
+            let index = products.findIndex(product => product.id == id);
+            if (index > -1) {
+                products[index] = product;
+                await fs.promises.writeFile(productsFilePath, JSON.stringify(products, null, '\t'));
+                return { status: "success", data: products[index] };
+            } else return { status: "error", message: "Producto no encontrado" };
+        } else return { status: "error", message: "No hay productos cargados" };
+    }
+    deleteProduct = async (id) => {
+        if (!id) return { status: "error", message: "Id no encontrado" };
+        if (fs.existsSync(productsFilePath)) {
+            let data = await fs.promises.readFile(productsFilePath, 'utf-8');
+            let products = JSON.parse(data);
+            let index = products.findIndex(product => product.id == id);
+            if (index > -1) {
+                products.splice(index, 1);
+                await fs.promises.writeFile(productsFilePath, JSON.stringify(products, null, '\t'));
+                return { status: "success", data: products };
+            } else return { status: "error", message: "Producto no encontrado" };
+        } else return { status: "error", message: "No hay productos cargados" };
+    }
+}
+        
+module.exports = ProductManager;
---
+++ Segunda-Entrega-ProyectoFinal/src/router/cartRouter.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const Manager = require('../controllers/cartManager');
+const router = express.Router();
+const manager = new Manager();
+ 
+router.post('/', (req, res) => {
+    manager.createCart(req.body)
+        .then((data) => {
+            res.send(data);
+        })
+        .catch((err) => {
+            res.status(404).send(err);
+        });
+});
+
+router.post('/:id/productos', (req, res) => {
+    manager.addProductToCart(req.params.id, req.body)
+        .then((data) => {
+            res.send(data);
+            })
+            .catch((err) => {
+                res.status(404).send(err);
+                });
+                });
+                
+router.delete('/:id', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.status(400).send({ error: 'El id debe ser un número' });
+    } else {
+        manager.deleteCart(req.params.id)
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                res.status(404).send(err);
+            });
+    }
+
+});
+router.delete('/:id/productos/:productId', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.status(400).send({ error: 'El id debe ser un número' });
+    } else {
+        manager.deleteProductFromCart(req.params.id, req.params.productId)
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                res.status(404).send(err);
+            });
+    }
+});
+router.get('/:id/productos', (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.status(400).send({ error: 'El id debe ser un número' });
+    } else {
+        manager.getCart(req.params.id)
+            .then((data) => {
+                res.send(data);
+            })
+            .catch((err) => {
+                res.status(404).send(err);
+            });
+    }
+});
+
+module.exports = router;
